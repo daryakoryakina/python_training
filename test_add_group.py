deda59f5e33f4824b813a1272c85d05f8e66c05c
dariakoriakina <d9046472289@gmail.com>
2019-04-24 18:45:26 +0300

    ДЗ 2: Вспомогательные методы и классыё

--- test_add_group.py
+++ test_add_group.py
@@ -1,53 +1,63 @@
-# -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+# from selenium.webdriver.common.by import By
+import unittest
+from group import Group
+
 
 class LoginTypeTest(unittest.TestCase):
+
+
+
     def setUp(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
 
-    
-    def test_login_type(self):
-        wd = self.wd
+    def method_name(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_groups(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd, grour):
+        # click to create new group
         wd.find_element_by_name("new").click()
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("eresartse")
+        wd.find_element_by_name("group_name").send_keys(grour.group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("erer")
+        wd.find_element_by_name("group_header").send_keys(grour.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("redr")
+        wd.find_element_by_name("group_footer").send_keys(grour.footer)
+        # click to creat group button
         wd.find_element_by_name("submit").click()
+
+    def return_to_groups(self, wd):
         wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
+
+    def test_login_type(self):
+        wd = self.wd
+        self.method_name(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups(wd)
+        self.create_group(wd, Group(group_name="zdfsdf", header="sdfzdfg", footer="zfgcfghcfh"))
+        self.return_to_groups(wd)
+        self.logout(wd)
+
 
     def tearDown(self):
         self.wd.quit()
