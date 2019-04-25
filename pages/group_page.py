ae3ce948494a7a69f0567eb429f834bc6021feaa
dariakoriakina <d9046472289@gmail.com>
2019-04-25 18:39:42 +0300

    Домашнее задание 2: test add number

--- pages/group_page.py
+++ pages/group_page.py
@@ -7,6 +7,7 @@ class GroupPage(BasePage):
     USER_FIELD = (By.XPATH, "//input[@name = 'user']")
     PASS_FIELD = (By.NAME, "pass")
     AUTH_BUTTON = (By.XPATH, "//input[@value='Login']")
+    LOGOUT = (By.LINK_TEXT, "Logout")
 
     def login(self, driver, username):
         group_page = GroupPage(driver)
@@ -18,5 +19,12 @@ class GroupPage(BasePage):
         group_page = GroupPage(driver)
         group_page.clear_input(self.PASS_FIELD)
         group_page.type_in(self.PASS_FIELD, password)
+
+    def auth_click(self, driver):
+        group_page = GroupPage(driver)
         group_page.click_on(self.AUTH_BUTTON)
+
+    def logout_1(self, driver):
+        group_page = GroupPage(driver)
+        group_page.click_on(self.LOGOUT)
 
