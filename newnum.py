1bfe106b933c91d4165037cdea5d5dee828f15b8
dariakoriakina <d9046472289@gmail.com>
2019-04-26 17:49:25 +0300

    Домашнее задание 2: добавлены параметры в один метод

--- newnum.py
+++ newnum.py
@@ -1,5 +1,7 @@
 class NewNum:
 
-    def __init__(self, first_name, middle_name):
+    def __init__(self, first_name, middle_name, address2, nickname):
         self.first_name = first_name
         self.middle_name = middle_name
+        self.address2 = address2
+        self.nickname = nickname
--- test_add_number.py
+++ test_add_number.py
@@ -30,31 +30,21 @@ class TestAddContact(unittest.TestCase, GroupPage):
         self.driver.find_element_by_name("middlename").click()
         self.driver.find_element_by_name("middlename").clear()
         self.driver.find_element_by_name("middlename").send_keys(newnum.middle_name)
-
-
-    def type_lastname(self, lastname):
-        self.driver.find_element_by_name("lastname").click()
-        self.driver.find_element_by_name("lastname").clear()
-        self.driver.find_element_by_name("lastname").send_keys(lastname)
-
-
-    def type_nickname(self, nickname):
         self.driver.find_element_by_name("nickname").click()
         self.driver.find_element_by_name("nickname").clear()
-        self.driver.find_element_by_name("nickname").send_keys(nickname)
-
-    def type_address2(self, address2):
+        self.driver.find_element_by_name("nickname").send_keys(newnum.nickname)
         self.driver.find_element_by_name("address2").click()
         self.driver.find_element_by_name("address2").clear()
-        self.driver.find_element_by_name("address2").send_keys(address2)
+        self.driver.find_element_by_name("address2").send_keys(newnum.address2)
 
     def click_save(self):
         self.driver.find_element_by_xpath("//input[@value='Enter']").click()
         self.driver.find_element_by_link_text("home page").click()
 
 
-    #def click_home_page_link(self):
-        #self.driver.find_element_by_link_text("home page").click()
+    def click_home_page_link(self):
+        self.driver.find_element_by_link_text("home page").click()
+        self.click_home_page_link()
 
     def logout(self, driver):
         group_page = GroupPage(driver)
@@ -63,9 +53,7 @@ class TestAddContact(unittest.TestCase, GroupPage):
     def test_add_contact(self):
         driver = self.driver
         self.auth(driver, username="admin", password="secret")
-        self.type_firstname(NewNum(first_name="dfggtxf", middle_name="edszertgderxde"))
-        self.type_nickname(nickname="fgxhgcfj")
-        self.type_address2(address2="sdfzdfgxdhgcfj")
+        self.type_firstname(NewNum(first_name="dfggtxf", middle_name="edszertgderxde", nickname="fgxhgcfj", address2="sdfzdfgxdhgcfj"))
         self.click_save()
         self.logout(driver)
         time.sleep(2)
