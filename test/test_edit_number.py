737f8f5a430e28409abb1bc430d7ca65e9b5c0cb
dariakoriakina <d9046472289@gmail.com>
2019-05-16 18:13:07 +0300

    исправлены параметры на один класс Number

--- test/test_edit_number.py
+++ test/test_edit_number.py
@@ -3,6 +3,6 @@ from model.number import Number
 
 def test_edit_contact(app):
     app.open_page()
-    app.number.edit_number(Number(second_name="1234567", nickname="123456"))
+    app.number.edit_number(Number(second_name="1234567", nickname="123456", address2="ffgxdhxfjx", middle_name="fgdzsxhgcxfjh", first_name="sgfdxzdhxf"))
 
 
