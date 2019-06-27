39f82418bb2343a20dca0fb569d0c7671070cfe8
Darya <d9046472289@gmail.com>
2019-06-27 13:42:11 +0300

    начало контактов

--- test/test_add_number.py
+++ test/test_add_number.py
@@ -5,8 +5,11 @@ from model.number import Number
 def test_add_contact(app):
     app.open_page()
     app.number.open_number_page()
+    old_number = app.number.get_number_list()
     app.number.create_number(Number(second_name="dgfxdghcfghm", first_name="dfggtxf", middle_name="edszertgderxde", nickname="fgxhgcfj", address2="sdfzdfgxdhgcfj"))
+    new_number = app.number.get_number_list()
     app.number.return_to_home_page()
+    assert len(old_number) +1 == len(new_number)
 
 
 
