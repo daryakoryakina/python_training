66081fc7f23b3e4007c86c1dcf06c2df1f48bdd0
Darya <d9046472289@gmail.com>
2019-06-28 13:30:53 +0300

    задание 11 (2)

--- test/test_add_number.py
+++ test/test_add_number.py
@@ -6,9 +6,12 @@ def test_add_contact(app):
     app.open_page()
     app.number.open_number_page()
     old_number = app.number.get_number_list()
-    app.number.create_number(Number(second_name="dgfxdghcfghm", first_name="dfggtxf", middle_name="edszertgderxde", nickname="fgxhgcfj", address2="sdfzdfgxdhgcfj"))
+    number = Number(second_name="dgfxdghcfghm", first_name="dfggtxf", middle_name="edszertgderxde", nickname="fgxhgcfj", address2="sdfzdfgxdhgcfj")
+    app.number.create_number(number)
     new_number = app.number.get_number_list()
     assert len(old_number) + 1 == len(new_number)
+    old_number.append(number)
+    assert sorted(old_number, key=Number.id_or_max) == sorted(new_number, key=Number.id_or_max)
 
 
 
