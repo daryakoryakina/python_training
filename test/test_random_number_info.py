44005f099dff139522ce29792a400a69d0943d24
Darya <d9046472289@gmail.com>
2019-07-05 22:10:44 +0300

    14 error TypeError: 'NoneType' object is not subscriptable

--- test/test_random_number_info.py
+++ test/test_random_number_info.py
@@ -21,18 +21,20 @@ def test_get_and_assert_random_number(app):
     assert contact_info_from_home_page.lastname == contact_info_from_edit_page.lastname
     assert contact_info_from_home_page.address == contact_info_from_edit_page.address
     assert contact_info_from_home_page.all_emails == merge_emails(contact_info_from_edit_page)
-    assert contact_info_from_home_page.all_phones_from_home_page == merge_phones_on_home_page(contact_info_from_edit_page)
+    assert contact_info_from_home_page.all_phones_from_home_page == merge_phones_on_home_page(
+        contact_info_from_edit_page)
 
 
 def clear(s):
-   return re.sub("[() -]", "", s)
+    return re.sub("[() -]", "", s)
 
 
 def merge_phones_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                           map(lambda x: clear(x),
-                               filter(lambda x: x is not None,
-                                      [contact.home_tel, contact.mobile_tel, contact.work_tel]))))
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.homephone, contact.mobilephone, contact.workphone,
+                                        contact.secondaryphone]))))
 
 
 def merge_emails(contact):
